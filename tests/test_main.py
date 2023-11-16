c57fe562ef656ba36857286b91eb986d8445b226
Daniil Sagalov <danielsagel@outlook.com>
2023-11-16 23:06:43 +0300

    HW-08 Long-deserved test refactoring in test_main.py: grouped repeating logic into common checks

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,23 +1,49 @@
+from typing import Literal
+
+import pytest
+from httpx import Response
+
 from app.main import app
 from fastapi.testclient import TestClient
 
-test_client: TestClient = TestClient(app)
+
+@pytest.fixture
+def test_client() -> TestClient:
+    return TestClient(app)
 
 
-def test_root_message_ok() -> None:
-    response = test_client.get("/")
-    assert response.status_code == 200
-    assert "application/json" in response.headers["content-type"]
+HttpMethod = Literal["POST", "GET", "PUT", "PATCH"]
+
+
+def test_root_message_ok(test_client: TestClient) -> None:
+    response: Response = run_base_response_checks("GET", "/", 200, test_client)
     assert response.json()["message"] == "Hello! This is the fraud detector."
 
 
-def test_number_of_entries_ok() -> None:
-    response = test_client.get("/number_of_entries")
-    assert response.status_code == 200
-    assert "application/json" in response.headers["content-type"]
+@pytest.mark.parametrize(
+    ("http_method", "method_url", "expected_status"),
+    [
+        ("GET", "/number_of_entries", 200),
+        ("POST", "/number_of_entries", 405),
+    ]
+)
+def test_simple_api_structure(
+        http_method: HttpMethod,
+        method_url: str,
+        expected_status: int,
+        test_client: TestClient
+) -> None:
+    run_base_response_checks(http_method, method_url, expected_status, test_client)
 
 
-def test_number_of_entries_not_allowed() -> None:
-    response = test_client.post("/number_of_entries")
-    assert response.status_code == 405
+def run_base_response_checks(
+        http_method: HttpMethod,
+        method_url: str,
+        expected_status: int,
+        test_client: TestClient
+) -> Response:
+    response: Response = test_client.request(method=http_method, url=method_url)
+    assert response.status_code == expected_status
     assert "application/json" in response.headers["content-type"]
+
+    return response
