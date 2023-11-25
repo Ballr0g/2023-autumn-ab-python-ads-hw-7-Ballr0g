640883b8e4f3049851492a1632ebc56b5a70bb07
Daniil Sagalov <danielsagel@outlook.com>
2023-11-25 15:42:15 +0300

    HW-08 Fixed mypy errors for test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import os
-from typing import Literal
+from typing import Literal, Any, Generator
 
 import pytest
 from app import models
@@ -17,7 +17,7 @@ class FixedPostgresContainer(PostgresContainer):
     """
     This wrapper type was the only way to fix testcontainers indefinitely loading on Windows...
     """
-    def get_connection_url(self, host=None):
+    def get_connection_url(self, host: Any = None) -> str:
         if os.name == "nt":
             return super().get_connection_url().replace("localnpipe", "localhost")
         return super().get_connection_url()
@@ -29,20 +29,29 @@ def postgres_version() -> str:
 
 
 @pytest.fixture(scope="module")
-def postgres_test_session(postgres_version: str) -> sessionmaker[Session]:
-    with FixedPostgresContainer(postgres_version) as pg_container:
-        pg_engine = create_engine(pg_container.get_connection_url())
-        models.Base.metadata.create_all(bind=pg_engine)
+def postgres_test_container(postgres_version: str) -> PostgresContainer:
+    container: PostgresContainer = FixedPostgresContainer(postgres_version)
+    try:
+        container.start()
+        yield container
+    finally:
+        container.stop()
 
-        pg_session_local: sessionmaker[Session] = sessionmaker(
-            autocommit=False, autoflush=False, bind=pg_engine
-        )
-        yield pg_session_local
+
+@pytest.fixture(scope="module")
+def postgres_test_session(postgres_test_container: PostgresContainer) -> sessionmaker[Session]:
+    pg_engine = create_engine(postgres_test_container.get_connection_url())
+    models.Base.metadata.create_all(bind=pg_engine)
+
+    pg_session_local: sessionmaker[Session] = sessionmaker(
+        autocommit=False, autoflush=False, bind=pg_engine
+    )
+    return pg_session_local
 
 
 @pytest.fixture(scope="module")
 def postgres_test_app(postgres_test_session: sessionmaker[Session]) -> FastAPI:
-    def get_db_test_override():
+    def get_db_test_override() -> Generator[Session, Any, None]:
         db = postgres_test_session()
         try:
             yield db
